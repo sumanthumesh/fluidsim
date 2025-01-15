0c0a15dd9338fdb9647f99b2ab76aca973858c98
Sumanth <sumanthu@umich.edu>
2025-01-14 20:38:40 -0500

    Single particle velocity and acceleration implemented. Issues with collision

--- fluidsim.py
+++ fluidsim.py
@@ -1,7 +1,10 @@
 import pygame
+import time
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+
 
 class Pair:
 
@@ -9,34 +12,80 @@ class Pair:
         self.x=x
         self.y=y
 
+    def tuple(self):
+        return (self.x,self.y)
+    
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
+    def round(self):
+        return (int(self.x),int(self.y))
+
 class Container:
 
-    def __init__(self,width,height):
+    def __init__(self,width:float,height:float,surface:pygame.Surface):
 
-        self.anchor = (200,200)
+        self.anchor:Pair = Pair(100,100)
         self.width = width
         self.height = height
+        self.surface=surface
+
+    def draw(self):
+        pygame.draw.rect(self.surface,(0,0,255),(self.anchor.x,self.anchor.y,self.width,self.height))
     
 class Particle:
 
-    def __init__(self,posx,posy):
+    def __init__(self,pos:Pair,v:Pair,radius:float,surface:pygame.Surface,e=0.8):
 
-        self.x=posx
-        self.y=posy
+        self.pos=pos
+        self.v:Pair=v
+        self.surface=surface
+        self.radius=radius
+        self.e=e
 
-    def move(acceleration:Pair,delta_time:):
+    def move(self,acc:Pair,delta_time:float):
 
+        #Update position
+        self.pos.x += self.v.x*delta_time
+        self.pos.y += self.v.y*delta_time
 
+        #Update velocity
+        self.v.x += acc.x*delta_time
+        self.v.y += acc.y*delta_time
 
+    def draw(self):
+
+        #Draw a circle of small radius
+        pygame.draw.circle(self.surface,(0,255,0),self.pos.tuple(),self.radius)        
+
+    def collide_with_boundary(self,pos:Pair,height,width):
+
+        #Check if we hit boundary
+        if self.pos.x - self.radius < pos.x or self.pos.x + self.radius > pos.x+width:
+            self.v.x = -0.9 * self.v.x
+        if self.pos.y - self.radius < pos.y or self.pos.y + self.radius > pos.y+height:
+            self.v.y = -0.9 * self.v.y
+
+        # print(f"{self.pos},({pos.x+width},{pos.y+height})")
+        print(f"{self.v.tuple()}")
 
 if __name__ == "__main__":
+
+
 
     # Initialize Pygame
     pygame.init()
 
     # Create a screen (window)
     screen = pygame.display.set_mode((800, 600))  # Width: 800, Height: 600
+    # Create the container
+    container = Container(200,100,screen)
     pygame.display.set_caption("My Pygame Window")
+
+    #Create particle
+    p = Particle(Pair(150,150),Pair(10,10),10,screen)
+    
+    time_stamp = time.time()
 
     # Main loop
     running = True
@@ -49,7 +98,16 @@ if __name__ == "__main__":
         screen.fill((255, 255, 255))
 
         # Draw a red rectangle (x, y, width, height)
-        pygame.draw.rect(screen, (255, 0, 0), (100, 100, 200, 150))
+        # pygame.draw.rect(screen, (255, 0, 0), (100, 100, 200, 150))
+        container.draw()
+
+        curr_time = time.time()
+        elapsed = curr_time - time_stamp
+        time_stamp = curr_time
+
+        p.move(Pair(0,9.8),elapsed)
+        p.collide_with_boundary(container.anchor,container.height,container.width)
+        p.draw()
 
         # Update the display
         pygame.display.flip()
